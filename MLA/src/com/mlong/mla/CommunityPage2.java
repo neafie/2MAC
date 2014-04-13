aa3dab2c88525c964c2d4f09006cdedcce422517
Aaron Gravelle <ajg10j@my.fsu.edu>
2014-04-13 00:53:50 -0400

    Go back to community page 1 when previous achievement is tapped.

--- MLA/src/com/mlong/mla/CommunityPage2.java
+++ MLA/src/com/mlong/mla/CommunityPage2.java
@@ -3,7 +3,9 @@ package com.mlong.mla;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link android.support.v4.app.Fragment} subclass.
@@ -11,14 +13,30 @@ import android.view.ViewGroup;
  */
 public class CommunityPage2 extends BaseFragment {
 
+	View view;
+	private TextView prevAch;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_community_page2, container,
+		view = inflater.inflate(R.layout.fragment_community_page2, container,
 				false);
+		
+		prevAch = (TextView) view.findViewById(R.id.gotopage2);
+		prevAch.setOnClickListener(listener);
+		
+		return view;
 	}
 
+	private OnClickListener listener = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			 mActivity.pushFragments(AppConstants.TAB_C, new CommunityPage1(),true,true);
+		}
+	};
 	
 	
 }
