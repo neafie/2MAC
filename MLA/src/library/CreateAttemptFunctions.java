f83091c4f0676d09c545824889761a947d648684
Aaron Gravelle <ajg10j@my.fsu.edu>
2014-04-14 13:35:37 -0400

    Merging

--- MLA/src/library/CreateAttemptFunctions.java
+++ MLA/src/library/CreateAttemptFunctions.java
@@ -12,13 +12,13 @@ public class CreateAttemptFunctions {
 	private JSONParser jsonParser;
 	private static String url_all_lists = "http://gza.bellwethersystems.com/vie_api/include/create_attempt.php";
 
-	public ListFunctions() {
+	public CreateAttemptFunctions() {
 		jsonParser = new JSONParser();
 	}
 
 	public JSONObject createList(String uid_creator, String aid_belongsto, String pos_votes, String neg_votes, String picture_link, String video_link) {
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		params.add(new BasicNameValuePair("lid_belongsto", lid_belongsto));
+		params.add(new BasicNameValuePair("lid_belongsto", aid_belongsto));
 		params.add(new BasicNameValuePair("uid_creator", uid_creator));
 		params.add(new BasicNameValuePair("ach_name", ach_name));
 		params.add(new BasicNameValuePair("startTime", startTime));
