74daaf2835f480bb7a9210ca64ce4f7138177c01
OwensC <neocorey@gmail.com>
2014-04-14 13:25:55 -0400

    Added DB functions
    Create an achievement
    Create an attempt
    Get all attempts

---
+++ MLA/src/library/CreateAttemptFunctions.java
@@ -0,0 +1,32 @@
+package library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+public class CreateAttemptFunctions {
+
+	private JSONParser jsonParser;
+	private static String url_all_lists = "http://gza.bellwethersystems.com/vie_api/include/create_attempt.php";
+
+	public ListFunctions() {
+		jsonParser = new JSONParser();
+	}
+
+	public JSONObject createList(String uid_creator, String aid_belongsto, String pos_votes, String neg_votes, String picture_link, String video_link) {
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("lid_belongsto", lid_belongsto));
+		params.add(new BasicNameValuePair("uid_creator", uid_creator));
+		params.add(new BasicNameValuePair("ach_name", ach_name));
+		params.add(new BasicNameValuePair("startTime", startTime));
+		params.add(new BasicNameValuePair("endTime", endTime));
+		params.add(new BasicNameValuePair("points", points));
+		JSONObject json = jsonParser.getJSONFromUrl(url_all_lists,params);
+
+		return json;
+	}
+
+}
---
+++ MLA/src/library/GetAttemptsFunction.java
@@ -0,0 +1,27 @@
+package library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+public class GetAttemptsFunctions {
+
+	private JSONParser jsonParser;
+	private static String url_all_lists = "http://gza.bellwethersystems.com/vie_api/include/get_all_attempts.php";
+
+	public ListFunctions() {
+		jsonParser = new JSONParser();
+	}
+
+	public JSONObject createList(String aid) {
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("aid", aid));
+		JSONObject json = jsonParser.getJSONFromUrl(url_all_lists,params);
+
+		return json;
+	}
+
+}
